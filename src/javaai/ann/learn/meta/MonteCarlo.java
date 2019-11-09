467f0564e18338b35dfda22c8d2d2f54321a2bc5
Ron.Coleman <ronncoleman@gmail.com>
2019-11-09 10:26:43 -0500

    Uupdate the monte carlo code.

--- src/javaai/ann/learn/meta/MonteCarlo.java
+++ src/javaai/ann/learn/meta/MonteCarlo.java
@@ -25,138 +25,143 @@ package javaai.ann.learn.meta;
 import java.util.Random;
 
 /**
- * This class uses MC simulation for unsupervised learning to solve y = (x-3)^2.
- * @author Ron.Coleman
+ * This class uses Monte Carlo simulation for unsupervised learning to maximize:
+ * f(x,y)=e^-(x^2 + y^2) + 2 * e^-((x-1.7)^2 + (y-1.7)^2).
+ * @see "<a href=https://en.wikipedia.org/wiki/Hill_climbing>Hill climbing</a>"
+ * @see "<a href=http://modelai.gettysburg.edu/2014/mc1/index.html>An Introduction to Monte Carlo Techniques in Artificial Intelligence - Part I</a>"
  */
+
 public class MonteCarlo {
     /** Convergence threshold */
     public final static double TOLERANCE = 0.01;
 
-    /** Maximum guess */
-    public final static int MAX = 128;
+    /** Number of samples */
+    public final static int NUM_SAMPLES = 10000;
 
-    /** Minimum guess */
-    public final static int MIN = 0;
+    /** Maximum guess constraint */
+    public final static double MAX = 10.0;
 
-    /** Total sample size */
-    public final static int SAMPLE_SIZE = 128;
+    /** Minimum guess constraint */
+    public final static double MIN = -10.0;
 
     /** Convergence criteria: number of times best solution stays best */
     public final static int MAX_SAME_COUNT = 5;
 
-    /** Random sample container */
-    double[] samples = null;
+    /** X index in samples array */
+    public final static int X = 0;
+
+    /** Y index in samples array */
+    public final static int Y = 1;
+
+    /** Z index in samples array */
+    public final static int Z = 2;
 
     /** Random number generator */
-    Random ran = new Random(0);
+    Random ran = new Random();
 
     /** Same count for test of convergence */
     protected int sameCount = 0;
 
-    /** Last y value of training iteration */
-    protected double yLast;
+    /** Last z value of training iteration */
+    protected double zLast;
 
     /**
-     * Launch point of app.
+     * App launch point
      * @param args Command line arguments
      */
     public static void main(String[] args) {
-        MonteCarlo parabola = new MonteCarlo();
+        MonteCarlo mc = new MonteCarlo();
 
-        double best = parabola.solve();
-        System.out.println("best = "+best);
+        double[] best = mc.solve();
+
+        System.out.printf("best x=%6.4f y=%6.4f z=%f\n",best[X], best[Y], best[Z]);
     }
 
     /**
      * Solves the equation.
      * @return Heuristic best solution
      */
-    public double solve() {
+    public double[] solve() {
         // Epoch count
         int iteration = 0;
 
-        // Current best y
-        double besty = Double.MAX_VALUE;
+        // Initial sample
+        double[] sample = {getSample(), getSample(), -Double.MAX_VALUE};
 
-        // Corresponding best x
-        double bestx = 0;
+        // Current best
+        double[] best = {sample[X], sample[Y], evaluate(sample)};
 
-        boolean converged = false;
+        // Write report header.
+        System.out.printf("%-3s  %6s  %6s  %6s  %5s\n","#", "X", "Y", "Z", "same");
 
         // Loop until solution converges
+        boolean converged = false;
+
         while(!converged) {
             iteration++;
 
-            double y  = Double.MAX_VALUE;
+            // Evaluate each sample in turn
+            for(int k = 0; k < NUM_SAMPLES; k++) {
+                sample[X] = getSample();
+                sample[Y] = getSample();
 
-            // Randomly sample the space
-            randomize();
+                sample[Z] = evaluate(sample);
 
-            // Test each sample in turn
-            for(int k = 0; k < SAMPLE_SIZE; k++) {
-                // Compute y = predict(x)
-                double x = samples[k];
-
-                y = getFitness(x);
-
-                // If this y is better than the best, update the y and x
-                if(y < besty) {
-                    besty = y;
-                    bestx = samples[k];
-                }
+                // If this z is better than the best, update best
+                if(sample[Z] > best[Z])
+                    System.arraycopy(sample,0,best,0,best.length);
             }
 
-            System.out.printf("%d y=%4.2f same=%d >> %s\n",iteration, besty, sameCount, bestx);
+            System.out.printf("%-3d  %6.4f  %6.4f  %6.4f  %5d\n",iteration,best[X],best[Y],best[Z], sameCount);
 
             // Check for convergence
-            converged = didConverge(besty);
+            converged = didConverge(best[Z]);
         }
 
-        return bestx;
+        return best;
     }
 
+    /**
+     * Gets a random sample
+     * @return Sample
+     */
+    protected double getSample() {
+        double sample = ran.nextDouble()*(MAX - MIN) + MIN;
+
+        return sample;
+    }
 
     /**
      * Objective function
-     * @param x Parameter
+     * @param sample Parameters
      * @return y=predict(x)
      */
-    protected double getFitness(double x) {
-        return (x-3)*(x-3);
+    protected double evaluate(double[] sample) {
+        double x = sample[X];
+        double y = sample[Y];
+
+        double z = Math.exp(-(x*x + y*y)) + 2.0 * Math.exp(-((x-1.7)*(x-1.7) + (y-1.7)*(y-1.7)));
+
+        return z;
     }
 
     /**
      * Convergence test function
-     * @param y Criteria
+     * @param z Criteria
      * @return True if the function converged, false otherwise
      */
-    protected boolean didConverge(double y) {
+    protected boolean didConverge(double z) {
         if(sameCount >= MAX_SAME_COUNT)
             return true;
 
-        if(Math.abs(yLast - y) < TOLERANCE) {
+        if(Math.abs(zLast - z) < TOLERANCE) {
             sameCount++;
         }
         else
             sameCount = 0;
 
-        yLast = y;
+        zLast = z;
 
         return false;
-    }
-
-    /**
-     * Randomly samples with replacement.
-     */
-    protected void randomize() {
-        // Initialize the samples
-        samples = new double[SAMPLE_SIZE];
-
-        // Draw n samples with replacement
-        for(int k = 0; k < SAMPLE_SIZE; k++) {
-            int sample = ran.nextInt(MAX - MIN) + MIN;
-
-            samples[k] = sample;
-        }
     }
 }
