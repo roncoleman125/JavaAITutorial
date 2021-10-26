18bdda20755b4039789833acf2363a9bf3342d03
Ron.Coleman <ronncoleman@gmail.com>
2021-10-26 14:57:28 -0400

    Improved the javadoc

--- src/main/java/javaai/metah/sa/City.java
+++ src/main/java/javaai/metah/sa/City.java
@@ -1,24 +1,67 @@
+/*
+ Copyright (c) Ron Coleman
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the
+ "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to
+ the following conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package javaai.metah.sa;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Represents a city.
+ * @author Ron.Coleman
+ */
 public class City {
+    /** Maximum range between cities in x or y coordinate. */
+    public final static int RANGE = 20;
+
+    private static Random ran = new Random(0);
 
     private int x;
     private int y;
 
+    /**
+     * Constructor
+     * @param x X coordinate
+     * @param y Y coordinate
+     */
     public City(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    // Getters and toString()
+
+    /**
+     * Gets the x coordinate.
+     * @return X
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Gets the y coordinate.
+     * @return Y
+     */
     public int getY() {
         return y;
     }
@@ -31,7 +74,12 @@ public class City {
         return city.x == this.x && city.y == this.y;
     }
 
-    public static List<City> get(int n) {
+    /**
+     * Makes a collection of cities at random locations.
+     * @param n Number of cities to make
+     * @return List of cities
+     */
+    public static List<City> make(int n) {
         List<City> cities = new ArrayList<>();
         for(int i=0; i < n; i++) {
             City city = getCity(cities);
@@ -40,8 +88,11 @@ public class City {
         return cities;
     }
 
-    private final static int RANGE = 20;
-    private static Random ran = new Random(0);
+    /**
+     * Gets a unique city that is not already in the list.
+     * @param cities Collection of cities in the list
+     * @return Unique city
+     */
     private static City getCity(List<City> cities) {
         int x = ran.nextInt(RANGE)+1;
         int y = ran.nextInt(RANGE)+1;
